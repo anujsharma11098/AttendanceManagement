b7ea9ed259a95abaa085819ae210ec919ae20e96
Anuj Sharma <48546869+anujsharma11098@users.noreply.github.com>
2020-08-17 20:49:23 +0530

    Update attendance.js

---
+++ routes/attendance.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const router = express.Router()
+
+const Attendance = require('../models/attendance')
+
+
+router.post('/', async (req, res) => {
+    const { Rollno, subject, date, status } = req.body
+    
+    console.log(req.body)
+    try {
+        await Attendance.create({
+            Rollno, subject, date, status: 0
+        })
+        res.status(201).json({ status: 201, message: 'Attendance Registered Successfully!' })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ status: 500, error: err })
+    }
+})
+
+
+
+module.exports = router
